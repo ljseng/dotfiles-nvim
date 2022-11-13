3ef7b89f97c41fa373f21d5bd66a45895377f867
ljseng <sengleejun@gmail.com>
2022-11-13 17:12:30 +0800

    Add barbar plugin configuration
    Also require packer_init before anything else so that
    plugin configuration can works properly.

--- init.lua
+++ init.lua
@@ -1,10 +1,11 @@
+-- initialize packer
+require('packer_init')
+
 require('core/options')
 require('core/keymaps')
 
 -- plugin specific configuration
 require('plugins/nvim-tree-cfg')
 require('plugins/lualine-cfg')
-
--- initialize packer
-require('packer_init')
+require('plugins/barbar-cfg')
 
