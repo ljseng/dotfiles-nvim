fa96eff8f446dbebfb6aaf28373be18d14fc3a23
ljseng <sengleejun@gmail.com>
2022-11-14 06:32:24 +0800

    Configure barbar plugin

--- lua/plugins/barbar-cfg.lua
+++ lua/plugins/barbar-cfg.lua
@@ -1,4 +1,7 @@
 require('bufferline').setup {
     animation = false,
-    auto_hide = true
+    auto_hide = true,
+    icon_pinned = '',
+    icon_close_tab = '',
+    icon_close_tab_modified = ''
 }
