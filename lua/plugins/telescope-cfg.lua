340bf6a919ba7fc3fe269ed97813de0527e361bc
ljseng <sengleejun@gmail.com>
2023-06-06 14:52:34 +0800

    change telescope layout strategy to vertical
    picker window usually seeing long name. vertical is more suitable

--- lua/plugins/telescope-cfg.lua
+++ lua/plugins/telescope-cfg.lua
@@ -1,4 +1,10 @@
 local telescope = require('telescope')
 
+telescope.setup {
+    defaults = {
+        layout_strategy = 'vertical',
+    },
+}
+
 telescope.load_extension('aerial')
 telescope.load_extension('notify')
