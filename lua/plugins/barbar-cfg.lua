1e0893f0434772c247e90adb643fd129329f1751
ljseng <sengleejun@gmail.com>
2023-06-08 02:25:55 +0800

    revert "remove customized devicons from barbar buffer line"
    this reverts commit 32160f6cc5c0b0a41764a05716975943465d3fe0.

--- lua/plugins/barbar-cfg.lua
+++ lua/plugins/barbar-cfg.lua
@@ -5,6 +5,8 @@ require('bufferline').setup {
     auto_hide = true,
     icons = {
         buffer_index = true,        -- show buffer index for easier jump around buffers
+        button = '',                                -- close button
+        modified = {button = '󰛉'},
         pinned = {button = '', filename = true},
         filetype = {
             enabled = false,        -- disable icon for filetype
