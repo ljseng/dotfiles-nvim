17c14a9e0b56fc600af9fb2dc3bc42ae668b473d
ljseng <sengleejun@gmail.com>
2023-06-08 11:42:27 +0800

    disable file icon in telescope

--- lua/plugins/telescope-cfg.lua
+++ lua/plugins/telescope-cfg.lua
@@ -4,6 +4,17 @@ telescope.setup {
     defaults = {
         layout_strategy = 'vertical',
     },
+    pickers = {
+        find_files = {
+            disable_devicons = true,
+        },
+        live_grep = {
+            disable_devicons = true,
+        },
+        buffers = {
+            disable_devicons = true,
+        },
+    },
 }
 
 telescope.load_extension('aerial')
