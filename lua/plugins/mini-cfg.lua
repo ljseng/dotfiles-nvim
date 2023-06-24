c6fffbed3af3e0589809d38674b6072ce299cc82
ljseng <sengleejun@gmail.com>
2023-06-25 01:17:09 +0800

    add mini starter screen and enable mini sessions

--- lua/plugins/mini-cfg.lua
+++ lua/plugins/mini-cfg.lua
@@ -1,4 +1,19 @@
+require('mini.sessions').setup({
+    autowrite = false,
+    file = 'session.vim',
+})
+
+local starter = require('mini.starter')
+starter.setup({
+    items = {
+        starter.sections.recent_files(15, true),    -- current directory, show max 10
+        starter.sections.recent_files(5, false),    -- global directory, show max 5
+        starter.sections.sessions(5, true),
+    },
+})
+
 require('mini.align').setup()
+
 require('mini.splitjoin').setup({
   mappings = {
     toggle = 'gs',
