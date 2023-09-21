a8d2ed946f6cfc0f8c7cf500a78e0c97eec35c90
ljseng <sengleejun@gmail.com>
2023-09-22 00:37:22 +0800

    add more keymaps working on git hunks

--- lua/plugins/gitsigns-cfg.lua
+++ lua/plugins/gitsigns-cfg.lua
@@ -10,22 +10,33 @@ require('gitsigns').setup {
 
         -- keymaps
         -- go to next hunk
-        map('n', '<leader>h.', function()
+        map('n', ']h', function()
             if vim.wo.diff then return '<leader>h.' end
             vim.schedule(function() gs.next_hunk() end)
             return '<Ignore>'
         end, {expr=true})
 
         -- go to previous hunk
-        map('n', '<leader>h,', function()
+        map('n', '[h', function()
             if vim.wo.diff then return '<leader>h,' end
             vim.schedule(function() gs.prev_hunk() end)
             return '<Ignore>'
         end, {expr=true})
 
-        map('n', '<leader>hr', gs.reset_hunk)
+        -- reset changes
+        map('n', '<leader>hr', gs.reset_hunk)       -- reset a hunk
+        map('n', '<leader>hR', gs.reset_buffer)     -- reset entire buffer
+
+        -- staging
+        map('n', '<leader>hs', gs.stage_hunk)       -- stage hunk
+        map('n', '<leader>hS', gs.stage_buffer)     -- stage entire buffer
+
+        -- diff
+        map('n', '<leader>hd', gs.diffthis)                         -- diff current version changes
+        map('n', '<leader>hD', function() gs.diffthis('~') end)     -- diff current version versus immediate previous version
+
+        -- misc
         map('n', '<leader>hp', gs.preview_hunk)
         map('n', '<leader>hb', function() gs.blame_line{ full = true } end)
-        map('n', '<leader>hd', gs.diffthis)
     end
 }
