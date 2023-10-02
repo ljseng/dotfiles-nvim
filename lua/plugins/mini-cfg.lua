969e2eab3de5809bf95de916c51f20eee18bca31
ljseng <sengleejun@gmail.com>
2023-10-03 00:29:40 +0800

    feat: Remove mini.starter
    It slows down the startup and I am not using the shortcuts a lot.

--- lua/plugins/mini-cfg.lua
+++ lua/plugins/mini-cfg.lua
@@ -3,15 +3,6 @@ require('mini.sessions').setup({
     file = 'session.vim',
 })
 
-local starter = require('mini.starter')
-starter.setup({
-    items = {
-        starter.sections.recent_files(15, true),    -- current directory, show max 10
-        starter.sections.recent_files(5, false),    -- global directory, show max 5
-        starter.sections.sessions(5, true),
-    },
-})
-
 require('mini.align').setup()
 
 require('mini.splitjoin').setup({
