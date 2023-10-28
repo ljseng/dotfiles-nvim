d65d208d4ac09f2b76988756b0e6251811931d3e
ljseng <sengleejun@gmail.com>
2023-10-29 01:22:07 +0800

    feat: Improve keymaps for navigating marks
    Also remove built-in marks. Find it disturbing rather than helping.

--- lua/plugins/marks-cfg.lua
+++ lua/plugins/marks-cfg.lua
@@ -1,8 +1,6 @@
 require('marks').setup {
     -- whether to map keybinds or not. default true
     default_mappings = true,
-    -- which builtin marks to show. default {}
-    builtin_marks = { "." },
     -- whether movements cycle back to the beginning/end of buffer. default true
     cyclic = true,
     -- whether the shada file is updated after modifying uppercase marks. default false
@@ -31,7 +29,9 @@ require('marks').setup {
         annotate = false,
     },
     mappings = {
-        toggle = "m,",
-        set_next = "m;"
+        toggle   = "m,",
+        next     = "]m",
+        prev     = "[m",
+        set_next = "m;",
     }
 }
