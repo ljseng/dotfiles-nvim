69e08326e6d265866987417c77b4e0561e56aae4
ljseng <sengleejun@gmail.com>
2023-11-05 01:20:18 +0800

    feat: Customize lualine to full potential
    Previously remove the barbar plugin. Found out to be quite needed.
    Lualine can do the barbar plugin job. Make list of buffers appear at
    statusline. Buffers at statusline will have buffer number. To ease
    switching to buffer quicker without needing to open buffer list. Helpful
    when not opening many buffers. When open many buffers, such that
    statusline has not enough space to display the buffer name properly,
    resort to telescope buffers.
    
    Make good use of tabline. Show code symbols, diagnostics and tab number
    here.
    
    fix: Disable lualine in oil and toggleterm

--- lua/plugins/lualine-cfg.lua
+++ lua/plugins/lualine-cfg.lua
@@ -5,18 +5,37 @@ end
 require('lualine').setup {
     options = {
         icons_enabled = false,
-        disabled_filetypes = { 'NvimTree' } -- do not show lualine in these places
+        disabled_filetypes = {
+            'NvimTree', 'oil', 'toggleterm',
+        },
+    },
+    tabline = {
+        lualine_a = { { 'filename', }, },
+        lualine_b = { { 'aerial', }, },
+        lualine_c = { { 'diagnostics' } },
+        lualine_x = { },
+        lualine_y = { },
+        lualine_z = { { 'tabs', }, },
     },
     sections = {
-        lualine_a = { { 'mode', fmt = get_str_first_char } },
-        lualine_c = {
-            'filename',
-            'aerial'
+        lualine_a = {
+            { 'mode', fmt = get_str_first_char, },
+            { 'branch' },
         },
+        lualine_b = {
+            { 'buffers', mode = 4 },
+        },
+        lualine_c = { },
+        lualine_x = { 'searchcount' },
+        lualine_y = { 'encoding', 'fileformat', },
+        lualine_z = { 'location', },
     },
     inactive_sections = {
-        lualine_c = {
-            'filename',
-        },
+        lualine_a = { },
+        lualine_b = { },
+        lualine_c = { 'filename', },
+        lualine_x = { },
+        lualine_y = { },
+        lualine_z = { 'location', },
     },
 }
