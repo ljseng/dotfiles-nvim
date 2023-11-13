8c19419032b75425d684a34a46539daf4a0bdea6
ljseng <sengleejun@gmail.com>
2023-11-13 15:22:09 +0800

    feat: Hit <esc> go to normal mode in term
    Not only term open by toggleterm. Term open by `:term` can benefit too.

--- lua/plugins/toggleterm-cfg.lua
+++ lua/plugins/toggleterm-cfg.lua
@@ -31,3 +31,10 @@ v.api.nvim_set_keymap("n", "<A-1>", "<cmd>lua LAZYGIT_TOGGLE()<CR>", {noremap = 
 v.api.nvim_set_keymap("n", "<leader>th", "<cmd>ToggleTerm direction=horizontal<CR>", {noremap = true, silent = true, desc = 'Toggleterm horizontal'})
 v.api.nvim_set_keymap("n", "<leader>tv", "<cmd>ToggleTerm direction=vertical<CR>", {noremap = true, silent = true, desc = 'Toggleterm vertical'})
 v.api.nvim_set_keymap("n", "<leader>tt", "<cmd>ToggleTerm direction=float<CR>", {noremap = true, silent = true, desc = 'Toggleterm float'})
+
+function _G.set_terminal_keymaps()
+    local opts = {buffer = 0}
+    v.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts) -- hit <esc> in term shall go to normal mode
+end
+
+v.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
