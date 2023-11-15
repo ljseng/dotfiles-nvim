1f957f8b981961c2e7e81ae1677e57df60330ac2
ljseng <sengleejun@gmail.com>
2023-11-15 12:55:37 +0800

    feat: Add keymap to quick jump out of toggleterm window

--- lua/plugins/toggleterm-cfg.lua
+++ lua/plugins/toggleterm-cfg.lua
@@ -35,6 +35,11 @@ v.api.nvim_set_keymap("n", "<leader>tt", "<cmd>ToggleTerm direction=float name=d
 function _G.set_terminal_keymaps()
     local opts = {buffer = 0}
     v.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts) -- hit <esc> in term shall go to normal mode
+    v.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+    v.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+    v.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+    v.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+    v.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
 end
 
 v.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
