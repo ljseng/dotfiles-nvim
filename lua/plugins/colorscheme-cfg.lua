2bb0093a269e4bffacf469c298cd5676cb50b616
ljseng <sengleejun@gmail.com>
2023-11-19 16:40:12 +0800

    feat(colorscheme): Make current and next search word to have same highlight
    Less eye and mental fatigue.

--- lua/plugins/colorscheme-cfg.lua
+++ lua/plugins/colorscheme-cfg.lua
@@ -4,7 +4,12 @@ local cs = require('nordic')
 cs.setup({
     cursorline = {
         theme = 'light',
-    }
+    },
+    override = {
+        Search = {
+             link = 'IncSearch', -- prefer current and next search word to have same highlight
+        },
+    },
 })
 
 v.cmd('colorscheme nordic')
